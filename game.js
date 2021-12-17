f83f9d0571415d75918e079a8118c09af0fd1529
Epoza <jacobpittsley2@gmail.com>
2021-12-16 21:28:33 -0500

    Added player and computer points

--- game.js
+++ game.js
@@ -16,13 +16,17 @@ function computerPlay() {
 
 const playerSelection = 'rock';
 const computerSelection = computerPlay();
+let computerPoints = 0
+let playerPoints = 0
 
 function playRound(playerSelection, computerSelection){
 
     if (computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'paper' && playerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'paper'){
+        computerPoints += 1;
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
     else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock'){
+        playerPoints += 1;
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
     else if (computerSelection == playerSelection){
@@ -39,6 +43,20 @@ function game(){
         const computerSelection = computerPlay()
         console.log('Round: '+i)
         console.log(playRound(playerSelection, computerSelection));
+        if (i == 5){
+            if (playerPoints > computerPoints){
+                console.log(`WINNER WINNER! you had ${playerPoints} points and the computer had ${computerPoints} points`)
+            }
+            else if (playerPoints < computerPoints){
+                return console.log(`Aww you lost! you had ${playerPoints} points and the computer had ${computerPoints} points`)
+            }
+            else{
+                return console.log(`Tie you both had ${playerPoints} points`)
+            }
+        }
+        else{
+            continue
+        }
     }
 }
 game();
