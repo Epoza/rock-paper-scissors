54dbdc5adf43902ab6a628430babc7198ffd5450
Epoza <jacobpittsley2@gmail.com>
2022-01-08 20:02:28 -0500

    display points and result

--- game.js
+++ game.js
@@ -1,42 +1,50 @@
 let randomSelection = 0
-const displayUser = document.getElementById('player-choice')
+const displayPlayer = document.getElementById('player-choice')
 let playerSelection
-const computerSelection = computerPlay();
+let computerSelection
+let result
 const displayComputer = document.getElementById('computer-choice')
+const displayResult = document.getElementById('result')
 let computerPoints = 0
 let playerPoints = 0
+const displayPlayerPoints = document.getElementById('player-points')
+const displayComputerPoints = document.getElementById('computer-points')
+
 
 // Get a random number for computer
 function computerPlay() {
     let randomSelection = Math.floor(Math.random()*3);
     if (randomSelection === 0){
-        return 'rock';
+        computerSelection = 'rock';
     }
     else if (randomSelection === 1){
-        return 'paper';
+        computerSelection = 'paper';
     }
     else if (randomSelection === 2){
-        return 'scissors';
+        computerSelection = 'scissors';
     }
-    return console.log(randomSelection)
+    displayComputer.innerHTML = computerSelection
 }
 
 function playRound(playerSelection, computerSelection){
 
     if (computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'paper' && playerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'paper'){
         computerPoints += 1;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        result = `You Lose, ${computerSelection} beats ${playerSelection}!`;
     }
     else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock'){
         playerPoints += 1;
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        result = `You Win, ${playerSelection} beats ${computerSelection}!`;
     }
     else if (computerSelection == playerSelection){
-        return `Tie! you chose ${playerSelection} and computer also chose ${computerSelection}`;
+        result = `Tie, you both chose ${playerSelection}!`;
     }
     else{
-        return 'That is not a valid option, please try again.'
+        result = 'That is not a valid option, please try again.'
     }
+    displayResult.innerHTML = result
+    displayPlayerPoints.innerHTML = playerPoints
+    displayComputerPoints.innerHTML = computerPoints
 }
 
 function game(){
@@ -44,10 +52,10 @@ function game(){
 
     console.log(playRound(playerSelection, computerSelection));
         if (playerPoints > computerPoints){
-            console.log(`WINNER WINNER! you had ${playerPoints} points and the computer had ${computerPoints} points`)
+            console.log(`WINNER WINNER! You had ${playerPoints} points and the computer had ${computerPoints} points`)
         }
         else if (playerPoints < computerPoints){
-            return console.log(`Aww you lost! you had ${playerPoints} points and the computer had ${computerPoints} points`)
+            return console.log(`Aww you lost! You had ${playerPoints} points and the computer had ${computerPoints} points`)
         }
         else{
             return console.log(`Tie you both had ${playerPoints} points`)
@@ -59,5 +67,7 @@ const buttons = document.querySelectorAll('button')
 
 buttons.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     playerSelection = e.target.id
-    displayUser.innerHTML = playerSelection
+    displayPlayer.innerHTML = playerSelection
+    computerPlay()
+    playRound(playerSelection, computerSelection)
 }))
