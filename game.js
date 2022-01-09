cfda1fb1d52739e9f5ebcd79caa39e1bd4b93cab
Epoza <jacobpittsley2@gmail.com>
2022-01-08 23:13:05 -0500

    Completed, need to style with css

--- game.js
+++ game.js
@@ -3,12 +3,17 @@ const displayPlayer = document.getElementById('player-choice')
 let playerSelection
 let computerSelection
 let result
+let winner
 const displayComputer = document.getElementById('computer-choice')
 const displayResult = document.getElementById('result')
 let computerPoints = 0
 let playerPoints = 0
 const displayPlayerPoints = document.getElementById('player-points')
 const displayComputerPoints = document.getElementById('computer-points')
+const displayWinner = document.getElementById('winner-text')
+const openButton = document.getElementById('open-button') 
+const modalContainer = document.getElementById('modal-container') 
+const closeButton = document.getElementById('close-button')
 
 
 // Get a random number for computer
@@ -28,42 +33,56 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection){
 
-    if (computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'paper' && playerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'paper'){
-        computerPoints += 1;
-        result = `You Lose, ${computerSelection} beats ${playerSelection}!`;
-    }
-    else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock'){
-        playerPoints += 1;
-        result = `You Win, ${playerSelection} beats ${computerSelection}!`;
-    }
-    else if (computerSelection == playerSelection){
-        result = `Tie, you both chose ${playerSelection}!`;
+    if (computerPoints != 5 && playerPoints != 5){
+        if (computerSelection === 'rock' && playerSelection === 'scissors' || computerSelection === 'paper' && playerSelection === 'rock' || computerSelection === 'scissors' && playerSelection === 'paper'){
+            computerPoints += 1;
+            result = `You Lose, ${computerSelection} beats ${playerSelection}!`;
+        }
+        else if (computerSelection === 'rock' && playerSelection === 'paper' || computerSelection === 'paper' && playerSelection === 'scissors' || computerSelection === 'scissors' && playerSelection === 'rock'){
+            playerPoints += 1;
+            result = `You Win, ${playerSelection} beats ${computerSelection}!`;
+        }
+        else if (computerSelection == playerSelection){
+            result = `Tie, you both chose ${playerSelection}!`;
+        }
+        else{
+            result = 'That is not a valid option, please try again.'
+        }
     }
     else{
-        result = 'That is not a valid option, please try again.'
+        if (playerPoints > computerPoints){
+            winner = `WINNER WINNER! you had ${playerPoints} points and the computer had ${computerPoints} points`
+        }
+        else{
+            winner = `Aww you lost! you had ${playerPoints} points and the computer had ${computerPoints} points`
+        }
+        displayWinner.innerHTML = winner
+        // Show the modal when game is over
+        modalContainer.classList.add('show')
+
+        // close the modal when clicked
+        closeButton.addEventListener('click', () => {
+            modalContainer.classList.remove('show')
+        })
     }
     displayResult.innerHTML = result
     displayPlayerPoints.innerHTML = playerPoints
     displayComputerPoints.innerHTML = computerPoints
 }
 
-function game(){
-    const computerSelection = computerPlay()
 
-    console.log(playRound(playerSelection, computerSelection));
-        if (playerPoints > computerPoints){
-            console.log(`WINNER WINNER! You had ${playerPoints} points and the computer had ${computerPoints} points`)
-        }
-        else if (playerPoints < computerPoints){
-            return console.log(`Aww you lost! You had ${playerPoints} points and the computer had ${computerPoints} points`)
-        }
-        else{
-            return console.log(`Tie you both had ${playerPoints} points`)
-        }
-    }
+    // Show the modal when game is over
+    openButton.addEventListener('click', () => {
+        modalContainer.classList.add('show')
+    })
+
+    // close the modal when clicked
+    closeButton.addEventListener('click', () => {
+        modalContainer.classList.remove('show')
+    })
 
     // Listen for button click then display it
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.button')
 
 buttons.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     playerSelection = e.target.id
